c8da0ee0a74bd17a47aba1feff409a1dbcd7212b
prambourg <rambourg.pierre@gmail.com>
2026-02-07 14:18:16 +0100

    Update 2025 2025

--- cds/tests/endpoints/test_member.py
+++ cds/tests/endpoints/test_member.py
@@ -14,12 +14,12 @@ if TYPE_CHECKING:
 @pytest.mark.parametrize(
         ("limit", "offset", "expected", "length"),
         [
-            (None, None, {"limit": DEFAULT_LIMIT, "offset": 0, "total": 5, "year": 2025}, 5),
-            (0, 0, {"limit": DEFAULT_LIMIT, "offset": 0, "total": 5, "year": 2025}, 5),
-            (2, None, {"limit": 2, "offset": 0, "total": 5, "year": 2025}, 2),
-            (None, 3, {"limit": DEFAULT_LIMIT, "offset": 3, "total": 5, "year": 2025}, 2),
-            (3, 1, {"limit": 3, "offset": 1, "total": 5, "year": 2025}, 3),
-            (5, 4, {"limit": 5, "offset": 4, "total": 5, "year": 2025}, 1),
+            (None, None, {"limit": DEFAULT_LIMIT, "offset": 0, "total": 5, "year": 2026}, 5),
+            (0, 0, {"limit": DEFAULT_LIMIT, "offset": 0, "total": 5, "year": 2026}, 5),
+            (2, None, {"limit": 2, "offset": 0, "total": 5, "year": 2026}, 2),
+            (None, 3, {"limit": DEFAULT_LIMIT, "offset": 3, "total": 5, "year": 2026}, 2),
+            (3, 1, {"limit": 3, "offset": 1, "total": 5, "year": 2026}, 3),
+            (5, 4, {"limit": 5, "offset": 4, "total": 5, "year": 2026}, 1),
         ])
 def test_get_members(
     client: FlaskClient,
@@ -47,10 +47,10 @@ def test_get_members(
     assert len(response.json["members"]) == length
 
 
-def test_get_members_2024(client: FlaskClient, session_with_members: scoped_session) -> None:
-    response = client.get("/members/?year=2024")
+def test_get_members_2025(client: FlaskClient, session_with_members: scoped_session) -> None:
+    response = client.get("/members/?year=2025")
     assert response.status_code == 200, f"Response status code: {response.status_code}"
-    expected = {"limit": DEFAULT_LIMIT, "offset": 0, "total": 1, "year": 2024}
+    expected = {"limit": DEFAULT_LIMIT, "offset": 0, "total": 1, "year": 2025}
     assert response.json["pagination"] == expected, (
         f"Expected {expected} members, but got {response.json["pagination"]}."
     )
